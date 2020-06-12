acd53db5e25ba9cbf727fed13a876997e2347fbf
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-12 08:26:31 -0700

    knexfile added, server up and running

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const helmet = require('helmet')
+const projectsRouter = require('./projects/projectsRouter')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+
+// server.use('/api/projects', projectsRouter)
+
+server.get('/', (req, res) => {
+     res.status(200).json({
+          server: 'up'
+     })
+})
+
+module.exports = server
